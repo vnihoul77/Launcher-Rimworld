7a1e6030a7f9c5ee76af4eb8af0c4b65500a2b91
vnihoul77 <vnihoul77@gmail.com>
2016-01-28 20:31:05 +0100

    Def.

---
+++ Launcher Rimworld/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Launcher Rimworld")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Launcher Rimworld")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("89581d81-4269-4f02-98f9-e8b6380e3947")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
